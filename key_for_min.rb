1cf5317293fac6f7ec7fdb1f4d885183fab685b1
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 17:23:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,9 @@
 def key_for_min_value(name_hash)
   lowest = 999
   lowest_item = ""
+  if name_hash.length == 0
+    nil
+  end
   name_hash.collect do |item, price|
     if price < lowest
       lowest_item = item
