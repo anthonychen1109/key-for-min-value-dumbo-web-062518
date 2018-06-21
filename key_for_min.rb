300cd5c7c2f8f233595bdf111a1c59752f74820c
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 17:20:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest = 999
+  lowest_item = ""
+  name_hash.collect do |item, price|
+    if item[:price] < lowest
+      lowest = item[:price]
+      lowest_item = item
+    end
+  end
+  lowest_item
 end
