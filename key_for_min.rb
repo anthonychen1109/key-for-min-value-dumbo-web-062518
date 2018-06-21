19c33c5a226ca563bdcec5f62b5777b35c641466
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 17:25:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest = 999
+  lowest_item = ""
+  
+  if !name_hash.empty?
+    name_hash.collect do |item, price|
+      if price < lowest
+        lowest_item = item
+        lowest = price
+      end
+    end
+  else
+    return
+  end
+  lowest_item
 end
